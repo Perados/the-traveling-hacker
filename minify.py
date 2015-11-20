0f647c9e53511ec02d1fa2f13c1c46a15d41070f
Perados <d.moracespedes@gmail.com>
2015-11-21 02:14:55 +0700

    Fix contact page and change cover images

--- minify.py
+++ minify.py
@@ -3,15 +3,17 @@
 import requests
 from sys import argv
 
-original_css_file_path = argv[1]
-original_css_file = open(original_css_file_path)
-original_css_text = original_css_file.read() 
+original_file_path = argv[1]
+file_type = argv[2]
+original_file = open(original_file_path)
+original_text = original_file.read() 
+minifier = 'cssminifier' if file_type == 'css' else 'javascript-minifier'
 
-minified_css_text = requests.post('http://cssminifier.com/raw', {'input': original_css_text}).content
+minified_text = requests.post('http://' + minifier + '.com/raw', {'input': original_text}).content
 
-splitted = original_css_file_path.split('.')
-minified_css_file_path = splitted[0] + '.min.' +splitted[1] 
-minified_css_file = open(minified_css_file_path, 'wb')
-minified_css_file.write(minified_css_text)
-minified_css_file.close()
+splitted = original_file_path.split('.')
+minified_file_path = splitted[0] + '.min.' +splitted[1] 
+minified_file = open(minified_file_path, 'wb')
+minified_file.write(minified_text)
+minified_file.close()
 
--- thetravelinghacker/blog/views.py
+++ thetravelinghacker/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render_to_response
-
+from django.views.decorators.csrf import csrf_exempt
+from django.core.mail import send_mail
+from django.http import HttpResponse
 from thetravelinghacker.blog.models import Post
 
 
@@ -20,3 +22,19 @@ def posts(request, post_id):
 
 def contact(request):
     return render_to_response('contact.html')
+
+@csrf_exempt
+def send_contact_mail(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        subject = '[The Traveling Hacker] Message from ' + name + ': ' + email
+
+        send_mail(
+            subject,
+            message,
+            email,
+            ['d.moracespedes@gmail.com'],
+        )
+        return HttpResponse()
