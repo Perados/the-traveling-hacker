3a82d7da65cb12c06119dd106f7eed53b8443c16
Perados <d.moracespedes@gmail.com>
2016-01-26 21:33:28 +0700

    Add Django REST api tests

--- thetravelinghacker/twitter/tests.py
+++ thetravelinghacker/twitter/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+import requests
 
-# Create your tests here.
+from rest_framework import status
+from django.test import LiveServerTestCase
+
+class ReadUserTest(LiveServerTestCase):
+    def setUp(self):
+        self.handle = 'justinbieber'
+
+    def test_can_read_user_detail(self):
+        response = requests.get("http://localhost:8082/api/twitter-users/" + self.handle)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class ReadUserErrorTest(LiveServerTestCase):
+   def setUp(self):
+       self.handle = 'justin bieber'
+
+   def test_can_detect_non_existent_user(self):
+       response = requests.get("http://localhost:8082/api/twitter-users/" + self.handle)
+       self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+       self.assertTrue(response.json()['error'])
+
